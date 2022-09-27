15833c72e2f9fae97ec002867e9855bed35cc2d0
wanderson648 <testepedroca@gmail.com>
2022-09-27 14:49:20 -0300

    Tornar o botão interativo

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.btn_roll)
+
+        rollButton.setOnClickListener {
+            val resultTextView: TextView = findViewById(R.id.tv_number)
+            resultTextView.text = "6"
+        }
+
+
     }
 }
